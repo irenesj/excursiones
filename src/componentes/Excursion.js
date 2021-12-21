8fa78c73b14b512107f0ccfa9809c3a25c632e06
Irene San José <75340773+irenesj@users.noreply.github.com>
2021-12-21 13:00:11 +0100

    Minor updates

--- src/componentes/Excursion.js
+++ src/componentes/Excursion.js
@@ -5,14 +5,16 @@ import styles from '../css/Excursion.module.css';
 function Excursion(props){
 
     return(
+
         <div className={styles.excursion}>
-            <img src="" alt="Excursiones" align="left"/>
-            {props.nombre} <br/>
+            <img src="" alt="Excursiones"/>
+            <div className={styles.title}>{props.nombre}</div> <br/>
             Zona: {props.zona} <br/>
             Dificultad: {props.dificultad} <br/>
             Horas estimadas: {props.horas} <br/>
             Descripción: {props.descripcion} <br/><br/>
         </div>
+        
     );
 }
 
--- src/componentes/Excursions.js
+++ src/componentes/Excursions.js
@@ -18,7 +18,7 @@ function Excursions(){
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
         sunt in culpa qui officia deserunt mollit anim id est laborum."/>,
-        <Excursion nombre="Parque natural del Ponga" zona="Oeste" dificultad="Dificil" horas="6" descripcion="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        <Excursion nombre="Picos de Europa" zona="Oeste" dificultad="Dificil" horas="6" descripcion="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
         sunt in culpa qui officia deserunt mollit anim id est laborum."/>
