c67dd57f701a5d847ab82256ed6a683756645aba
Irene San José <75340773+irenesj@users.noreply.github.com>
2022-01-03 12:42:47 +0100

    Minor updates

--- src/componentes/User.js
+++ src/componentes/User.js
@@ -6,15 +6,15 @@ import styles from '../css/User.module.css';
 function User(){
 
     return(
-        <Container>
+        <Container className={styles.user}>
             <Row>
                 <Col xs="4">
                     <Image className={styles.userImage} src="../images/user-icon.svg" roundedCircle />
-                    Nombre
+                    Nombre y Apellidos
                 </Col>
                 <Col className={styles.userInfo} xs="8">
-                    <div>Excursiones favoritas</div>
-                    <div>Excursiones a las que has ido</div>
+                    <div className={styles.title}>Excursiones favoritas</div>
+                    <div className={styles.title}>Excursiones a las que has ido</div>
                     <Button variant="success">Añadir excursión</Button>
                 </Col>
             </Row>
