ae418491668fb3c4d9cb861d8e1285a9bf85b7e0
Irene San José <75340773+irenesj@users.noreply.github.com>
2022-01-11 17:50:30 +0100

    Update Filters.js

--- src/componentes/Filters.js
+++ src/componentes/Filters.js
@@ -10,8 +10,6 @@ function Filters(){
       <div>
         <ListGroup variant="flush">
         <ListGroup.Item className={styles.filters}>Picos de Europa</ListGroup.Item>
-        <ListGroup.Item className={styles.filters}>Cangas de Onís</ListGroup.Item>
-        <ListGroup.Item className={styles.filters}>Gijón</ListGroup.Item>
         <ListGroup.Item className={styles.filters}>Llanes</ListGroup.Item>
         <ListGroup.Item className={styles.filters}>Lastres</ListGroup.Item>
       </ListGroup>
