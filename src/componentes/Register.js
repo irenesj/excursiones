dee6b14344850453ef3a129d468330760605dfa8
Irene San José <75340773+irenesj@users.noreply.github.com>
2022-01-26 12:31:37 +0100

    Update Register.js

--- src/componentes/Register.js
+++ src/componentes/Register.js
@@ -22,6 +22,16 @@ function Register(){
 
     }
 
+    const mailChange = (event) => {
+
+
+    }
+
+    const passwordChange = (event) => {
+
+
+    }
+
     const [disabled, setDisabled] = useState(true);
 
     useEffect(() => {
@@ -37,150 +47,31 @@ function Register(){
                 <Row>
                     <Col xs="12">
                         <Form className={styles.form}>
-
-                        <Row className="mb-3">
+                            <Row className="mb-3">
                                 <Form.Group as={Col} controlId="formGridAddress1">
                                     <Form.Label>Nombre *</Form.Label>
                                     <Form.Control onKeyUp={nameChange}/>
                                 </Form.Group>
-
                                 <Form.Group as={Col} controlId="formGridAddress2">
                                     <Form.Label>Apellidos *</Form.Label>
                                     <Form.Control onKeyUp={surnameChange}/>
                                 </Form.Group>
                             </Row>
-
                             <Row className="mb-3">
-
                                 <Form.Group as={Col} controlId="formGridPhone">
                                     <Form.Label>Teléfono *</Form.Label>
                                     <Form.Control type="email" onKeyUp={phoneChange}/>
                                 </Form.Group>
-
                                 <Form.Group as={Col} controlId="formGridEmail">
                                     <Form.Label>Correo electrónico *</Form.Label>
-                                    <Form.Control type="email"/>
+                                    <Form.Control type="email" onKeyUp={mailChange}/>
                                 </Form.Group>
-
                                 <Form.Group as={Col} controlId="formGridPassword">
                                     <Form.Label>Contraseña *</Form.Label>
-                                    <Form.Control type="password"/>
+                                    <Form.Control type="password" onKeyUp={passwordChange}/>
                                 </Form.Group>
                             </Row>
-                           
-                            <Row className="mb-3">
-
-                                <Form.Group as={Col} controlId="formGridCity">
-                                    <Form.Label>Ciudad *</Form.Label>
-                                    <Form.Control/>
-                                </Form.Group>
-
-                                <Form.Group as={Col} controlId="formGridCommunity">
-                                    <Form.Label>Comunidad Autónoma *</Form.Label>
-                                    <Form.Select defaultValue="Elige...">
-                                        <option>Elige...</option>
-                                        <option>Andalucía</option>
-                                        <option>Aragón</option>
-                                        <option>Asturias</option>
-                                        <option>Cantabria</option>
-                                        <option>Castilla La Mancha</option>
-                                        <option>Castilla y León</option>
-                                        <option>Cataluña</option>
-                                        <option>Extremadura</option>
-                                        <option>Galicia</option>
-                                        <option>Islas Baleares</option>
-                                        <option>Canarias</option>
-                                        <option>La Rioja</option>
-                                        <option>Madrid</option>
-                                        <option>Murcia</option>
-                                        <option>Navarra</option>
-                                        <option>País Vasco</option>
-                                        <option>Valencia</option>
-                                    </Form.Select>
-                                </Form.Group>
-
-                                <Form.Group as={Col} controlId="formGridProvince">
-                                    <Form.Label>Provincia *</Form.Label>
-                                    <Form.Select defaultValue="Elige...">
-                                        <option>Elige...</option>
-                                        <optgroup label="Andalucía">
-                                            <option>Almería</option>
-                                            <option>Cádiz</option>
-                                            <option>Cordoba</option>
-                                            <option>Granada</option>
-                                            <option>Huelva</option>
-                                            <option>Jaén</option>
-                                            <option>Málaga</option>
-                                            <option>Sevilla</option>
-                                        </optgroup>
-                                        <optgroup label="Aragón">
-                                            <option>Huesca</option>
-                                            <option>Teruel</option>
-                                            <option>Zaragoza</option>
-                                        </optgroup>
-                                        <option>Asturias</option>
-                                        <option>Cantabria</option>
-                                        <optgroup label="Castilla La Mancha">
-                                            <option>Albacete</option>
-                                            <option>Ciudad Real</option>
-                                            <option>Cuenca</option>
-                                            <option>Guadalajara</option>
-                                            <option>Toledo</option>
-                                        </optgroup>
-                                        <optgroup label="Castilla y León">
-                                            <option>Ávila</option>
-                                            <option>Burgos</option>
-                                            <option>León</option>
-                                            <option>Palencia</option>
-                                            <option>Salamanca</option>
-                                            <option>Segovia</option>
-                                            <option>Soria</option>
-                                            <option>Valladolid</option>
-                                            <option>Zamora</option>
-                                        </optgroup>
-                                        <optgroup label="Cataluña">
-                                            <option>Barcelona</option>
-                                            <option>Gerona</option>
-                                            <option>Lérida</option>
-                                            <option>Tarragona</option>
-                                        </optgroup>
-                                        <optgroup label="Extremadura">
-                                            <option>Badajoz</option>
-                                            <option>Cáceres</option>
-                                        </optgroup>
-                                        <optgroup label="Galicia">
-                                            <option>La Coruña</option>
-                                            <option>Lugo</option>
-                                            <option>Orense</option>
-                                            <option>Pontevedra</option>
-                                        </optgroup>
-                                        <option>Islas Baleares</option>
-                                        <optgroup label="Canarias">
-                                            <option>Las Palmas</option>
-                                            <option>Santa Cruz de Tenerife</option>
-                                        </optgroup>
-                                        <option>La Rioja</option>
-                                        <option>Madrid</option>
-                                        <option>Murcia</option>
-                                        <option>Navarra</option>
-                                        <optgroup label="País Vasco">
-                                            <option>Álava</option>
-                                            <option>Guipúzcua</option>
-                                            <option>Vizcaya</option>
-                                        </optgroup>
-                                        <optgroup label="Valencia">
-                                            <option>Alicante</option>
-                                            <option>Castellón</option>
-                                            <option>Valencia</option>
-                                        </optgroup>
-                                    </Form.Select>
-                                </Form.Group>
-
-                                <Form.Group as={Col} controlId="formGridPostalCode">
-                                    <Form.Label>Código postal *</Form.Label>
-                                    <Form.Control/>
-                                </Form.Group>
-
+                            <Row>
                                 <Form.Group>
                                     <Form.Text className="text-muted">
                                     <ul className={styles.list}>
@@ -189,7 +80,6 @@ function Register(){
                                     </ul>
                                     </Form.Text>
                                 </Form.Group>
-
                             </Row>
                             <div className={styles.btn}>
                                 <Button variant="primary" type="submit" disabled={disabled}>
