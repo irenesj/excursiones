925c82bb96f7af017c1d2abb73da3c7ce7df05ba
Irene San José <75340773+irenesj@users.noreply.github.com>
2022-02-06 12:46:30 +0100

    Minor updates

--- src/componentes/Register.js
+++ src/componentes/Register.js
@@ -49,10 +49,8 @@ function Register(){
                                 <ValidatedFormGroup control="formGridPassword" name="Repite la contraseña *" inputToChange={setSamePassword}/>
                             </Row>
                             <Row>
-                                <ul className={styles.list}>
-                                    <li>Debes estar registrado para poder añadir y/o apuntarte a una excursión</li>
-                                    <li>Los campos que llevan el asterisco &#40;*&#41; son obligatorios</li>
-                                </ul>   
+                                <p className={styles.list}>Debes estar registrado para poder añadir y/o apuntarte a una excursión<br/>
+                                Los campos que llevan el asterisco &#40;*&#41; son obligatorios</p>
                             </Row>
                             <div className={styles.btn}>
                                 <Button variant="primary" type="submit" disabled={disabled}>
