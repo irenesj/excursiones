e0af3f7e1ade2b012164d7ed04417a5e269b9a59
Irene San José <75340773+irenesj@users.noreply.github.com>
2022-03-11 12:46:52 +0100

    More comments added

--- src/components/ValidatedFormGroup.js
+++ src/components/ValidatedFormGroup.js
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
 import { Col, Form } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
 import styles from '../css/ValidatedFormGroup.module.css';
 
 
 function ValidatedFormGroup(props){
 
-   const [notValid, setNotValid] = useState(false);
+    // Variable that saves if the information we receive is valid or not, (is not blank or in an incorrect format)
+    const [notValid, setNotValid] = useState(false);
 
+    // Function that receives the information and updates it
     const nameChange = (event) => {
+
         const value = event.target.value;
         props.inputToChange(value);
         setNotValid(!props.validationFunction(value));
  
     }
 
+
     return(
+
         <Form.Group as={Col} controlId={props.control}>
             <Form.Label>{props.name}</Form.Label>
             <Form.Control type={props.inputType} onChange={nameChange} value={props.value}/>
             { notValid && <p className={styles.errorMessage}>Recuerda, no puedes dejar un campo vacío o en un formato incorrecto</p> }
         </Form.Group>
+
     );
+
 }
 
 export default ValidatedFormGroup;
