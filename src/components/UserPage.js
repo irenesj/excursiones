1d9be5721672ca2ff819363c792e689ebe68f25f
Irene San José <75340773+irenesj@users.noreply.github.com>
2022-04-07 12:57:04 +0200

    When the user log outs the page goes to the landing page

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Col, Button } from 'react-bootstrap';
 import { useSelector } from "react-redux";
+import { Navigate } from "react-router";
 import 'bootstrap/dist/css/bootstrap.css';
 import styles from '../css/UserPage.module.css';
 
 
 function UserPage(){
 
-    const { user } = useSelector((state) => state.loginReducer);
+    const { login: isLoggedIn, user } = useSelector((state) => state.loginReducer);
+
+    if(!isLoggedIn) {
+        return <Navigate replace to='/'/>;
+    }
 
     return(
 
