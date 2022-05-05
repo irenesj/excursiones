bd9ed2d726b5c2616b92af64cde852089ec04f06
Irene San José <75340773+irenesj@users.noreply.github.com>
2022-05-05 18:20:01 +0200

    Starting with the filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Accordion } from 'react-bootstrap';
+import FiltersList from './FiltersList';
 import 'bootstrap/dist/css/bootstrap.css';
 import styles from '../css/Filters.module.css';
 
@@ -12,25 +13,19 @@ function Filters(){
           <Accordion.Item eventKey="0">
             <Accordion.Header>Zona</Accordion.Header>
             <Accordion.Body>
-              <ul className={styles.listInfo}>
-                
-              </ul>
+              <FiltersList />
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
             <Accordion.Header>Dificultad</Accordion.Header>
             <Accordion.Body>
-              <ul className={styles.listInfo}>
-               
-              </ul>
+              <FiltersList />
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
             <Accordion.Header>Tiempo estimado</Accordion.Header>
             <Accordion.Body>
-              <ul className={styles.listInfo}>
-                
-              </ul>
+              <FiltersList />
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
---
+++ src/components/FiltersList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import styles from '../css/FiltersList.module.css';
+
+function FiltersList(){
+
+    
+
+return (
+
+    <ul className={styles.listInfo}>
+        <li>Hola</li>     
+    </ul>
+
+);
+}
+
+export default FiltersList;
